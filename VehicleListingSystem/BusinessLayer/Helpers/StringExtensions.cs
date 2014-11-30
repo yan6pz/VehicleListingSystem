5acd90cdcda6edc57d8567f0fe985300e212f545
Yanislav Shterev <yan6pz@gmail.com>
2014-11-30 15:36:15 +0200

    Creating Vehicle Listing System Repository
    Some simple usage with xml data. Reading information about vehicles from
    xml(name model manufacturer, image) and visualizing it in the browser.
    Some filters on price name and manufacturer have been made.

---
+++ VehicleListingSystem/BusinessLayer/Helpers/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string GetFilePath(this string name)
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath((name));
+            var pattern = "VehicleListingSystem\\VehicleListingSystem\\";
+
+            var filePath = path.Replace(pattern, "VehicleListingSystem\\");
+            if (filePath.Contains("\\Vehicle\\"))
+            {
+                filePath = filePath.Replace("\\Vehicle\\", "\\");
+            }
+            return filePath;
+        }
+    }
+}
---
+++ VehicleListingSystem/BusinessLayer/IPopulateData.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public interface IPopulateData
+    {
+        IEnumerable<Vehicle> GetAllVehicles(string fileName);
+        Vehicle GetVehicleDetails(string fileName, string id);
+        IEnumerable<Vehicle> GetModels(string fileName,string manufacturer);
+        //IEnumerable<Vehicle> GetVehiclesFilter(string fileName, string model, string manufacturer, decimal price);
+    }
+}
