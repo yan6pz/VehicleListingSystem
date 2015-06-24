97a198e21d140cf8ffa84bd6016351b483141280
Yanislav Shterev <yan6pz@gmail.com>
2015-06-24 23:48:40 +0300

    final commit

--- BusinessLayer/Helpers/StringExtensions.cs
+++ BusinessLayer/Helpers/StringExtensions.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace BusinessLayer.Helpers
 {
@@ -13,15 +8,13 @@ namespace BusinessLayer.Helpers
         {
             var path = GetContentDirectory();
             path = path.Substring(0, path.Length - 22);
-            var filePath = Path.Combine(path, name);
+            var filePath = System.IO.Path.Combine(path, name);
             return filePath;
         }
         private static string GetContentDirectory()
         {
-
             var folderName = AppDomain.CurrentDomain.BaseDirectory;
-            //var contentPath = Path.Combine(folderName);
-         
+
             return folderName;
         }
     }
